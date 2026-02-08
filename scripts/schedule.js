a2c44a7115455e65904ccc518bdefde8997451f7
NXTITAN <158617734+NXTlTAN@users.noreply.github.com>
2026-02-09 01:06:43 +0300

    Add files via upload

---
+++ scripts/schedule.js
@@ -0,0 +1,20 @@
+async function loadXML() {
+  const response = await fetch('https://raw.githubusercontent.com/zzzzzzzzzzzz-del/redak54/refs/heads/main/schedule.xml');
+  const xmlText = await response.text();
+
+  const parser = new DOMParser();
+  const xmlDoc = parser.parseFromString(xmlText, "text/xml");
+
+  const names = xmlDoc.getElementsByTagName("name");
+  const homeworks = xmlDoc.getElementsByTagName("homework");
+
+  const objectLessons = document.getElementsByClassName("lesson");
+  const objectHomeworks = document.getElementsByClassName("homework");
+
+  for (let i = 0; i < 6; i++) {
+    objectLessons[i].innerHTML = names[i].textContent;
+    objectHomeworks[i].innerHTML = homeworks[i].textContent;
+  }
+}
+
+loadXML();
